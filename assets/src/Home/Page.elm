85f92557f3c4f1a3b16d86a34e1bfa606fc682f4
Kayser <zack.kayser@kroger.com>
2018-11-05 09:24:33 -0500

    Add container view body

--- assets/src/Home/Page.elm
+++ assets/src/Home/Page.elm
@@ -5,12 +5,13 @@ import Colors.Palette as Palette
 import Element exposing (Element, DeviceClass(..))
 import Element.Background as Background
 import Element.Border as Border
+import Element.Events as Events
 import Element.Font as Font
 import Element.Input as Input
 import Fonts
 import Html exposing (..)
 import Html.Events exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes as Attrs exposing (..)
 import Http
 import Json.Decode
 import Json.Encode
@@ -21,11 +22,13 @@ type alias Model =
   , formTitle : String
   , createdMessage : String
   , key : Nav.Key
+  , topics : List Topic
   }
 
 type alias Config =
   { title : String
   , borderColor : Element.Color
+  , addButtonColor : Element.Color
   }
 
 type alias Topic = { topic : String }
@@ -36,6 +39,7 @@ initialModel navKey =
   , formTitle = ""
   , createdMessage = ""
   , key = navKey
+  , topics = [ { topic = "The first topic" }, { topic = "The second topic" } ]
   }
 
 -- VIEW
@@ -56,13 +60,21 @@ viewPageLayout model =
     , Element.spacing 50
     ] 
     [ 
-     viewContainer model { borderColor = Palette.primaryLight, title = "Topics" }
-    , viewContainer model { borderColor = Palette.secondary, title = "Decks" }
+     viewContainer model 
+      { borderColor = Palette.primaryLight
+      , title = "Topics" 
+      , addButtonColor = Palette.secondaryDark
+      }
+    , viewContainer model 
+      { borderColor = Palette.secondary
+      , title = "Decks" 
+      , addButtonColor = Palette.primaryDark
+      }
     ]
 
 viewContainer : Model -> Config -> Element Msg
 viewContainer model config =
-    Element.row
+    Element.column
       [ Background.color config.borderColor
         , Element.height Element.fill
         , Element.width (Element.fillPortion 2)
@@ -74,38 +86,89 @@ viewContainer model config =
           }
         , Element.padding 16
       ]  
-      [ Element.row 
-        [ Element.height (Element.px 48)
-        , Element.width Element.fill
-        , Element.alignTop
-        ]
-        [ Element.el
-          [ Element.centerY
-          , Element.centerX
-          , Fonts.titleSize
-          , Font.medium
-          , Palette.whiteFont
-          ] (Element.text config.title ) 
-        , Input.button 
-          [ Background.color Palette.secondaryDark
-          , Border.shadow
-            { offset = ( 2.0, 2.0 )
-            , size = 1.0
-            , blur = 4.0
-            , color = Element.rgba 0.2 0.2 0.2 0.5
-            }
-          , Element.centerY
-          , Element.alignRight
-          , Border.rounded 100
-          , Element.moveDown 1.5
-          , Palette.whiteFont
-          ] 
-          { onPress = Just AddTopic
-          , label = Element.html ( Html.i [ class "material-icons" ] [ Html.text "add" ] )
-          }
-        ]
-      ] 
+      [ viewContainerTitleRow model config
+      , viewContainerBody model config
+      ]
+ 
 
+viewContainerTitleRow : Model -> Config -> Element Msg
+viewContainerTitleRow model config =
+  Element.row 
+    [ Element.height (Element.px 48)
+    , Element.width Element.fill
+    , Element.alignTop
+    ]
+    [ Element.el
+      [ Element.centerY
+      , Element.centerX
+      , Fonts.titleSize
+      , Font.medium
+      , Palette.whiteFont
+      ] ( Element.text config.title )
+    , Input.button
+      [ Background.color config.addButtonColor
+      , Border.shadow
+        { offset = ( 2.0, 2.0 )
+        , size = 1.0
+        , blur = 4.0
+        , color = Element.rgba 0.2 0.2 0.2 0.5
+        }
+      , Element.centerY
+      , Element.alignRight
+      , Border.rounded 100
+      , Element.moveDown 1.5
+      , Palette.whiteFont
+      ]
+      { onPress = Just AddTopic
+      , label = Element.html ( Html.i [ class "material-icons" ] [ Html.text "add" ] )
+      }
+    ]
+
+
+viewContainerBody : Model -> Config -> Element Msg
+viewContainerBody model config =
+  Element.row
+    [ Element.height (Element.px 48)
+    , Element.width Element.fill
+    , Element.centerY
+    , Element.centerX
+    , Element.moveUp 48.0
+    ]
+    [ Element.el
+      [ Element.alignLeft
+      , Palette.whiteFont
+      , Font.semiBold
+      , Events.onClick ClickedLeft 
+      , Element.pointer
+      ] (Element.html ( 
+        Html.i 
+          [ class "material-icons" 
+          , Attrs.style "font-size" "3rem"
+          ] [ Html.text "chevron_left" ] ) )
+    , Element.el 
+      [ Element.centerX
+      , Palette.whiteFont
+      , Font.size 24
+      , Font.medium
+      ] (Element.text (topicText model))
+    , Element.el
+      [ Element.alignRight
+      , Palette.whiteFont
+      , Font.semiBold
+      , Element.pointer
+      , Events.onClick ClickedRight
+      ] (Element.html ( 
+        Html.i 
+          [ class "material-icons" 
+          , Attrs.style "font-size" "3rem"
+          ] [ Html.text "chevron_right" ] ) ) 
+    ]
+
+topicText : Model -> String
+topicText model =
+  case model.topics of
+    [] -> "You don't have any topics yet."
+    topicRecord :: topics -> topicRecord.topic
 
 -- UPDATE
 
@@ -114,6 +177,8 @@ type Msg
   | FormSubmitted (Result Http.Error String)
   | SetFormTitle String
   | AddTopic
+  | ClickedLeft
+  | ClickedRight
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -141,6 +206,12 @@ update msg model =
         ( newModel, Cmd.none )
     AddTopic ->
       Debug.log ("Add topic button pressed")
+      ( model, Cmd.none )
+    ClickedLeft ->
+      Debug.log ("Clicked on the left chevron button")
+      ( model, Cmd.none )
+    ClickedRight ->
+      Debug.log ("Clicked on the right chevron button")
       ( model, Cmd.none )
 
 -- REQUEST
